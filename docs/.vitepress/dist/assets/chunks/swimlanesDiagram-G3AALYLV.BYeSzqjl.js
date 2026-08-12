7f1280bf1de7c982d1cdaa77d2a9b19c46dd44e3
AnAverageBeing <AnAverageBeing@users.noreply.github.com>
2026-08-13 00:57:35 +0530

    whats-new 2.17.0: junk-alert suppression bullet

--- docs/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.BYeSzqjl.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.Di6ehC-4.js";import{_ as a}from"../app.Dk7lg8yD.js";import"./chunk-5VM5RSS4.l9j3e1ZI.js";import"./chunk-XXDRQBXY.Dd9zTCNi.js";import"./chunk-VR4S4FIN.B0TyNWyu.js";import"./chunk-32BRIVSS.DYNVG6CD.js";import"./channel.CgNc2Clv.js";import"./framework.CZqXuAiL.js";import"./theme.DFPIQPuZ.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ docs/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.bwetZLhD.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.Nlc8_JpH.js";import{_ as a}from"../app.-nt8q4Oo.js";import"./chunk-5VM5RSS4.B3gs0xvr.js";import"./chunk-XXDRQBXY.CrCWZwna.js";import"./chunk-VR4S4FIN.BX1KMep3.js";import"./chunk-32BRIVSS.Css4X_U5.js";import"./channel.ByMRCzih.js";import"./framework.CZqXuAiL.js";import"./theme.DLMijWQT.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
