58941b3379f06ec2817f26a916adce892e5b5023
AnAverageBeing <AnAverageBeing@users.noreply.github.com>
2026-08-23 19:37:02 +0530

    rebuild dist (setup API docs)

---
+++ docs/.vitepress/dist/assets/chunks/channel.tgonMzE1.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.A4z8tZVr.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.Bh5qgqKg.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BwAa5j2q.js";import{_ as a}from"../app.A4z8tZVr.js";import"./chunk-5VM5RSS4.j0r5a722.js";import"./chunk-XXDRQBXY.9U3HyDjv.js";import"./chunk-VR4S4FIN.DD0bzudd.js";import"./chunk-32BRIVSS.CI3AQ5qI.js";import"./channel.tgonMzE1.js";import"./framework.CZqXuAiL.js";import"./theme.Bc9O6hTE.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
