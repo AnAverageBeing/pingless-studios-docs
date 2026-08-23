1b7cb6db86d81c358fa6b5b7911da4bf52050551
AnAverageBeing <AnAverageBeing@users.noreply.github.com>
2026-08-23 11:18:26 +0530

    rebuild dist (fixed broken @upsetjs/venn.js install)

---
+++ docs/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.ChZr0S-T.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.R-JYwwyO.js";import{_ as a}from"../app.DGpcrxzm.js";import"./chunk-5VM5RSS4.BzXpsYy1.js";import"./chunk-XXDRQBXY.DsNyKIeo.js";import"./chunk-VR4S4FIN.C0UMccgA.js";import"./chunk-32BRIVSS.CTDoR0x1.js";import"./channel.CJrco3aL.js";import"./framework.CZqXuAiL.js";import"./theme.Mj1_6lH9.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
