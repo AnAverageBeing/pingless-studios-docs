ac1e80d331107f3f97e5f20317277df7f7590018
AnAverageBeing <AnAverageBeing@users.noreply.github.com>
2026-09-18 14:43:19 +0530

    chore: rebuild dist (build 9 whats-new)

--- docs/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.Cf_1wmSj.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.Ditwttob.js";import{_ as a}from"../app.q46RLt4H.js";import"./chunk-5VM5RSS4.D5L9U7si.js";import"./chunk-XXDRQBXY.CRLHkvPQ.js";import"./chunk-VR4S4FIN.ZKyaUcoC.js";import"./chunk-32BRIVSS.0Qb9k3FU.js";import"./channel.zdvvWZ5L.js";import"./framework.CZqXuAiL.js";import"./theme.B9ZSen9T.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
